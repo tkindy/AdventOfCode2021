7048c093204a87dbeacf3754ba6216dcda4707fd
Tyler Kindy <tylerkindy@gmail.com>
2021-12-02 01:02:14 -0500

    Add day template

---
+++ template/src/dayXX.clj
@@ -0,0 +1,9 @@
+(ns dayXX)
+
+(defn parse-input [input])
+
+(defn read-input []
+  (parse-input (slurp "input")))
+
+(defn -main []
+  (let [... (read-input)]))
---
+++ template/test/dayXX_test.clj
@@ -0,0 +1,5 @@
+(ns dayXX-test
+  (:require dayXX
+            [clojure.test :refer [deftest is]]))
+
+(deftest parse-input)
