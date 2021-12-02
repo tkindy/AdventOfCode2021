284cbdda9fdfa2f450cac53dca1e1794a6d24ab4
Tyler Kindy <tylerkindy@gmail.com>
2021-12-02 00:10:31 -0500

    Parse input

--- day2/src/day2.clj
+++ day2/src/day2.clj
@@ -1,4 +1,15 @@
-(ns day2)
+(ns day2
+  (:require [clojure.string :refer [split]]))
+
+(defn parse-input [input]
+  (let [lines (split input #"\n")]
+    (map (fn [line]
+           (let [[direction distance] (split line #" ")]
+             [(symbol direction) (Integer/parseInt distance)]))
+         lines)))
+
+(defn read-input []
+  (parse-input (slurp "input")))
 
 (defn -main []
   (println "Hello, world!"))
--- day2/test/day2_test.clj
+++ day2/test/day2_test.clj
@@ -1,3 +1,7 @@
 (ns day2-test
   (:require day2
             [clojure.test :refer [is deftest]]))
+
+(deftest parse-input
+  (is (= (day2/parse-input "forward 5\ndown 3\ndown 2\nforward 7\nup 8\n")
+  [['forward 5] ['down 3] ['down 2] ['forward 7] ['up 8]])))
