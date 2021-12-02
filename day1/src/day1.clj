caa4a4b7921449b9ec26143d9197571a8b0232d0
Tyler Kindy <tylerkindy@gmail.com>
2021-12-01 22:39:19 -0500

    Add input parsing

--- day1/src/day1.clj
+++ day1/src/day1.clj
@@ -1,4 +1,12 @@
-(ns day1)
+(ns day1
+  (:require [clojure.string :refer [split]]))
+
+(defn parse-input [input]
+  (->> (split input #"\n")
+       (map #(Integer/parseInt %))))
+
+(defn read-input []
+  (parse-input (slurp "input")))
 
 (defn -main []
   (println "Hello, world!"))
