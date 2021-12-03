2aa80782c38f942c97482505e41c83b9bc2ffe1d
Tyler Kindy <tylerkindy@gmail.com>
2021-12-03 00:32:24 -0500

    Finish part 1

--- src/day03.clj
+++ src/day03.clj
@@ -10,5 +10,36 @@
 (defn read-input []
   (parse-input (slurp "inputs/day3")))
 
+(defn get-digits [i numbers]
+  (map #(nth % i) numbers))
+
+(defn most-common [digits]
+  (let [counts (reduce (fn [counts digit]
+                         (update counts digit
+                                 (fn [old] (if (nil? old) 1 (+ old 1)))))
+                       {}
+                       digits)]
+    (key (apply max-key val counts))))
+
+(defn most-common-digits [numbers]
+  (let [length (count (first numbers))]
+    (map (fn [i] (->> numbers
+                      (get-digits i)
+                      most-common))
+         (range length))))
+
+(defn bits->decimal [bits]
+  (Integer/parseInt (apply str bits) 2))
+
+(defn flip-bits [bits]
+  (map (fn [bit] (if (= 0 bit) 1 0)) bits))
+
+(defn part1 [numbers]
+  (let [most-common (most-common-digits numbers)
+        gamma (bits->decimal most-common)
+        epsilon (bits->decimal (flip-bits most-common))]
+    (* gamma epsilon)))
+
 (defn -main []
-  (let []))
+  (let [numbers (read-input)]
+    (println "Part 1: " (part1 numbers))))
--- test/day03_test.clj
+++ test/day03_test.clj
@@ -3,18 +3,26 @@
             [clojure.test :refer [deftest is]]))
 
 (def example-input "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010\n")
+(def example-numbers [[0 0 1 0 0]
+                      [1 1 1 1 0]
+                      [1 0 1 1 0]
+                      [1 0 1 1 1]
+                      [1 0 1 0 1]
+                      [0 1 1 1 1]
+                      [0 0 1 1 1]
+                      [1 1 1 0 0]
+                      [1 0 0 0 0]
+                      [1 1 0 0 1]
+                      [0 0 0 1 0]
+                      [0 1 0 1 0]])
 
 (deftest parse-input
-  (is (= (day03/parse-input example-input)
-         [[0 0 1 0 0]
-          [1 1 1 1 0]
-          [1 0 1 1 0]
-          [1 0 1 1 1]
-          [1 0 1 0 1]
-          [0 1 1 1 1]
-          [0 0 1 1 1]
-          [1 1 1 0 0]
-          [1 0 0 0 0]
-          [1 1 0 0 1]
-          [0 0 0 1 0]
-          [0 1 0 1 0]])))
+  (is (= (day03/parse-input example-input) example-numbers)))
+
+(deftest most-common
+  (is (= (day03/most-common [1]) 1))
+  (is (= (day03/most-common [0 1 0]) 0))
+  (is (= (day03/most-common [0 1 0 0 1 1 0]) 0)))
+
+(deftest part1
+  (is (= (day03/part1 example-numbers) 198)))
