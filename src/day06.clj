faa1e2b51d75b16d6fca68f02ad59b1ecd2b1d1c
Tyler Kindy <tylerkindy@gmail.com>
2021-12-06 08:29:29 -0500

    Finish part 2
    I had to make this more efficient; trying to run even the example for 256
    days with the old implementation was taking a huge amount of time. All
    the vectors and copying, I imagine.
    
    When first reading through the problem, I had the idea to use maps with
    just counts. But I went for the simpler solution first (that matched the
    example in the problem) and optimized later.

--- src/day06.clj
+++ src/day06.clj
@@ -2,31 +2,43 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (mapv #(Integer/parseInt (str/trim %)) (str/split input #",")))
+  (->> (str/split input #",")
+       (map #(Integer/parseInt (str/trim %)))
+       (map (fn [timer] {timer 1}))
+       (apply merge-with +)))
 
 (defn read-input []
   (parse-input (slurp "inputs/day06.txt")))
 
 (defn simulate1 [timers]
-  (let [[updated new]
-        (reduce (fn [[updated new] timer]
-                  (if
-                   (= timer 0)
-                    [(conj updated 6) (conj new 8)]
-                    [(conj updated (- timer 1)) new]))
-                [[] []]
-                timers)]
-    (into [] (concat updated new))))
+  (reduce
+   (fn [timers [timer count]]
+     (let [add #(+ (or % 0) count)]
+       (if (= timer 0)
+         (-> timers
+             (update 8 add)
+             (update 6 add))
+         (update timers (- timer 1) add))))
+   {}
+   timers))
 
 (defn simulate [timers days]
   (reduce (fn [timers _] (simulate1 timers))
           timers
           (range days)))
 
+(defn count-fish [timers]
+  (->> timers
+       (map val)
+       (apply +)))
+
 (defn part1 [timers]
-  (let [simulated (simulate timers 80)]
-    (count simulated)))
+  (count-fish (simulate timers 80)))
+
+(defn part2 [timers]
+  (count-fish (simulate timers 256)))
 
 (defn -main []
   (let [timers (read-input)]
-    (println "Part 1: " (part1 timers))))
+    (println "Part 1: " (part1 timers))
+    (println "Part 2: " (part2 timers))))
--- test/day06_test.clj
+++ test/day06_test.clj
@@ -3,17 +3,20 @@
             [clojure.test :refer [deftest is]]))
 
 (def example-input "3,4,3,1,2")
-(def example-parsed [3 4 3 1 2])
+(def example-parsed {3 2, 4 1, 1 1, 2 1})
 
 (deftest parse-input
   (is (= (d/parse-input example-input) example-parsed)))
 
 (deftest simulate
   (is (= (d/simulate example-parsed 0) example-parsed))
-  (is (= (d/simulate example-parsed 1) [2 3 2 0 1]))
-  (is (= (d/simulate example-parsed 2) [1 2 1 6 0 8]))
-  (is (= (d/simulate example-parsed 3) [0 1 0 5 6 7 8]))
-  (is (= (d/simulate example-parsed 18) [6 0 6 4 5 6 0 1 1 2 6 0 1 1 1 2 2 3 3 4 6 7 8 8 8 8])))
+  (is (= (d/simulate example-parsed 1) {2 2, 3 1, 0 1, 1 1}))
+  (is (= (d/simulate example-parsed 2) {1 2, 2 1, 6 1, 0 1, 8 1}))
+  (is (= (d/simulate example-parsed 3) {0 2, 1 1, 5 1, 6 1, 7 1, 8 1}))
+  (is (= (d/simulate example-parsed 18) {0 3, 7 1, 1 5, 4 2, 6 5, 3 2, 2 3, 5 1, 8 4})))
 
 (deftest part1
   (is (= (d/part1 example-parsed) 5934)))
+
+(deftest part2
+  (is (= (d/part2 example-parsed) 26984457539)))
