568e6116f1275533e3e84ca792aa34faed5279b7
Tyler Kindy <tylerkindy@gmail.com>
2021-12-12 07:21:35 -0500

    Implement parsing

--- src/day12.clj
+++ src/day12.clj
@@ -1,8 +1,17 @@
 (ns day12
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
+
+(defn parse-line [line]
+  (let [[a b] (map keyword (str/split line #"-"))]
+    {a #{b}
+     b #{a}}))
 
 (defn parse-input [input]
-  {})
+  (->> input
+       str/split-lines
+       (map parse-line)
+       (apply merge-with set/union)))
 
 (defn read-input []
   (parse-input (slurp "inputs/day12.txt")))
--- test/day12_test.clj
+++ test/day12_test.clj
@@ -5,3 +5,12 @@
 (def example1-input (slurp "examples/day12-1.txt"))
 (def example2-input (slurp "examples/day12-2.txt"))
 (def example3-input (slurp "examples/day12-3.txt"))
+
+(deftest parse-input
+  (is (= (d/parse-input example1-input)
+         {:start #{:A :b}
+          :A     #{:start :b :c :end}
+          :b     #{:start :A :d :end}
+          :c     #{:A}
+          :d     #{:b}
+          :end   #{:A :b}})))
