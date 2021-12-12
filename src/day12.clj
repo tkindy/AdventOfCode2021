926974963996ae220859f5d2be93dbf8fb51033a
Tyler Kindy <tylerkindy@gmail.com>
2021-12-12 07:09:58 -0500

    Set up day 12

---
+++ src/day12.clj
@@ -0,0 +1,10 @@
+(ns day12
+  (:require [clojure.string :as str]))
+
+(defn parse-input [input]
+  {})
+
+(defn read-input []
+  (parse-input (slurp "inputs/day12.txt")))
+
+(defn -main [])
---
+++ test/day12_test.clj
@@ -0,0 +1,7 @@
+(ns day12-test
+  (:require [day12 :as d]
+            [clojure.test :refer [deftest is]]))
+
+(def example1-input (slurp "examples/day12-1.txt"))
+(def example2-input (slurp "examples/day12-2.txt"))
+(def example3-input (slurp "examples/day12-3.txt"))
