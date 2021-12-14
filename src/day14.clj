58dab2bc46740b7235c48bb5b933565a69468df8
Tyler Kindy <tylerkindy@gmail.com>
2021-12-14 07:47:11 -0500

    Finish part 1

--- src/day14.clj
+++ src/day14.clj
@@ -22,4 +22,29 @@
 (defn read-input []
   (parse-input (slurp "inputs/day14.txt")))
 
-(defn -main [])
+(defn step [chain rules]
+  (let [new-chain
+        (->> chain
+             (partition 2 1)
+             (reduce (fn [chain pair]
+                       (conj chain (first pair) (rules pair)))
+                     []))]
+    (conj new-chain (peek chain))))
+
+(defn count-elements [chain]
+  (->> chain
+       (map (fn [element] {element 1}))
+       (apply merge-with +)))
+
+(defn part1 [{:keys [template rules]}]
+  (let [chain (reduce (fn [chain _] (step chain rules))
+                      template
+                      (range 10))
+        freqs (count-elements chain)
+        min-count (apply min (vals freqs))
+        max-count (apply max (vals freqs))]
+    (- max-count min-count)))
+
+(defn -main []
+  (let [state (read-input)]
+    (println "Part 1:" (part1 state))))
--- test/day14_test.clj
+++ test/day14_test.clj
@@ -24,3 +24,16 @@
                   [\B \C] \B
                   [\C \C] \N
                   [\C \N] \C}})))
+
+(deftest step
+  (is (= (d/step [\N \N \C \B] (:rules example))
+         [\N \C \N \B \C \H \B]))
+  (is (= (d/step [\N \C \N \B \C \H \B] (:rules example))
+         [\N \B \C \C \N \B \B \B \C \B \H \C \B]))
+  (is (= (d/step [\N \B \C \C \N \B \B \B \C \B \H \C \B] (:rules example))
+         [\N \B \B \B \C \N \C \C \N \B \B \N \B \N \B \B \C \H \B \H \H \B \C \H \B]))
+  (is (= (d/step [\N \B \B \B \C \N \C \C \N \B \B \N \B \N \B \B \C \H \B \H \H \B \C \H \B] (:rules example))
+         (into [] "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"))))
+
+(deftest part1
+  (is (= (d/part1 example) 1588)))
