ec5d65cc1b5cb881b5802601621b3326e582c5c3
Tyler Kindy <tylerkindy@gmail.com>
2021-12-14 07:29:07 -0500

    Implement parsing

--- src/day14.clj
+++ src/day14.clj
@@ -1,8 +1,23 @@
 (ns day14
   (:require [clojure.string :as str]))
 
+(defn parse-template [template]
+  (into [] template))
+
+(defn parse-rule [rule]
+  (let [[pair result] (str/split rule #" -> ")]
+    {(into [] pair) (first result)}))
+
+(defn parse-rules [rules]
+  (->> rules
+       str/split-lines
+       (map parse-rule)
+       (apply merge)))
+
 (defn parse-input [input]
-  {})
+  (let [[template rules] (str/split input #"\n\n")]
+    {:template (parse-template template)
+     :rules (parse-rules rules)}))
 
 (defn read-input []
   (parse-input (slurp "inputs/day14.txt")))
