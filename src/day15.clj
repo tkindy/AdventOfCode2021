a562d0ef68cdd3e8687d1c9d7bbbd1cf846a54d1
Tyler Kindy <tylerkindy@gmail.com>
2021-12-15 07:29:37 -0500

    Implement parsing

--- src/day15.clj
+++ src/day15.clj
@@ -1,7 +1,11 @@
 (ns day15
   (:require [clojure.string :as str]))
 
-(defn parse-input [input])
+(defn parse-line [line]
+  (mapv #(Integer/parseInt (str %)) line))
+
+(defn parse-input [input]
+  (mapv parse-line (str/split-lines input)))
 
 (defn read-input []
   (parse-input (slurp "inputs/day15.txt")))
