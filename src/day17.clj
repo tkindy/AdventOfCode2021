0308714fd063d19cc445d19c59c860df7703593b
Tyler Kindy <tylerkindy@gmail.com>
2021-12-17 12:24:48 -0500

    Set up day 17

---
+++ src/day17.clj
@@ -0,0 +1,8 @@
+(ns day17)
+
+(defn parse-input [input])
+
+(defn read-input []
+  (parse-input (slurp "inputs/day17.txt")))
+
+(defn -main [])
---
+++ test/day17_test.clj
@@ -0,0 +1,5 @@
+(ns day17-test
+  (:require [day17 :as d]
+            [clojure.test :refer [deftest is]]))
+
+(def example-input "target area: x=20..30, y=-10..-5\n")
