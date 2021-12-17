3b5afd71be22733c4e494ff8e2079120e70d8b1a
Tyler Kindy <tylerkindy@gmail.com>
2021-12-17 11:21:49 -0500

    Add values to examples

--- test/day16_test.clj
+++ test/day16_test.clj
@@ -12,18 +12,25 @@
 
 (deftest parse-input
   (is (= (d/parse-input "D2FE28\n")
-         {:version 6}))
+         {:version 6
+          :value 2021}))
   (is (= (d/parse-input "38006F45291200\n")
          {:version 1
-          :subpackets [{:version 6}
-                       {:version 2}]}))
+          :subpackets [{:version 6
+                        :value 10}
+                       {:version 2
+                        :value 20}]}))
   (is (= (d/parse-input "EE00D40C823060\n")
          {:version 7
-          :subpackets [{:version 2}
-                       {:version 4}
-                       {:version 1}]}))
+          :subpackets [{:version 2
+                        :value 1}
+                       {:version 4
+                        :value 2}
+                       {:version 1
+                        :value 3}]}))
   (is (= (d/parse-input "8A004A801A8002F478\n")
          {:version 4
           :subpackets [{:version 1
                         :subpackets [{:version 5
-                                      :subpackets [{:version 6}]}]}]})))
+                                      :subpackets [{:version 6
+                                                    :value 15}]}]}]})))
