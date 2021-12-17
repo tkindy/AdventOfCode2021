0c388737e448e1ef728898320f27a51fad952745
Tyler Kindy <tylerkindy@gmail.com>
2021-12-17 11:03:28 -0500

    Start parsing

--- src/day16.clj
+++ src/day16.clj
@@ -1,8 +1,72 @@
-(ns day16
-  (:require [clojure.string :as str]))
+(ns day16)
+
+(defn decode-char [char]
+  (case char
+    \0 [0 0 0 0]
+    \1 [0 0 0 1]
+    \2 [0 0 1 0]
+    \3 [0 0 1 1]
+    \4 [0 1 0 0]
+    \5 [0 1 0 1]
+    \6 [0 1 1 0]
+    \7 [0 1 1 1]
+    \8 [1 0 0 0]
+    \9 [1 0 0 1]
+    \A [1 0 1 0]
+    \B [1 0 1 1]
+    \C [1 1 0 0]
+    \D [1 1 0 1]
+    \E [1 1 1 0]
+    \F [1 1 1 1]
+    \newline []))
+
+(defn decode [input]
+  (mapcat decode-char input))
+
+(defn bits->int [bits]
+  (Integer/parseInt (apply str bits) 2))
+
+(defn take-bits [n bits]
+  [(take n bits) (drop n bits)])
+
+(defn take-int [n bits]
+  (let [[int-bits bits] (take-bits n bits)]
+    [(bits->int int-bits) bits]))
 
 (defn parse-input [input]
-  )
+  (letfn [(parse-packet [bits]
+            (let [[{:keys [type], :as header} bits] (parse-header bits)
+                  body (if (= type 4)
+                         (parse-literal bits)
+                         (parse-operator bits))]
+              [(-> (merge header body)
+                   (dissoc :type))
+               bits]))
+          (parse-operator [bits]
+            (let [[length-type bits] (take-int 1 bits)]
+              (if (= length-type 0)
+                (parse-operator-by-length bits)
+                (parse-operator-by-count bits))))
+          ;; TODO
+          (parse-operator-by-length [bits]
+            (let [[length bits] (take-int 15 bits)
+                  bits (take length bits)
+                  subpackets (loop [subpackets []
+                                    bits bits]
+                               ())]))
+          ;; TODO
+          (parse-operator-by-count [bits]
+            {})
+          (parse-literal [bits]
+            {})
+          (parse-header [bits]
+            (let [[version bits] (take-int 3 bits)
+                  [type bits] (take-int 3 bits)]
+              [{:version version, :type type}, bits]))]
+    (->> input
+         decode
+         parse-packet
+         first)))
 
 (defn read-input []
   (parse-input (slurp "inputs/day16.txt")))
--- test/day16_test.clj
+++ test/day16_test.clj
@@ -2,5 +2,28 @@
   (:require [day16 :as d]
             [clojure.test :refer [deftest is]]))
 
-(def example-input (slurp "examples/day16.txt"))
-(def example (d/parse-input example-input))
+(deftest decode
+  (is (= (d/decode "D2FE28\n")
+         '(1 1 0 1 0 0 1 0 1 1 1 1
+             1 1 1 0 0 0 1 0 1 0 0 0))))
+
+(deftest bits->int
+  (is (= (d/bits->int [0 1 1 1 1 1 1 0 0 1 0 1]) 2021)))
+
+(deftest parse-input
+  (is (= (d/parse-input "D2FE28\n")
+         {:version 6}))
+  (is (= (d/parse-input "38006F45291200\n")
+         {:version 1
+          :subpackets [{:version 6}
+                       {:version 2}]}))
+  (is (= (d/parse-input "EE00D40C823060\n")
+         {:version 7
+          :subpackets [{:version 2}
+                       {:version 4}
+                       {:version 1}]}))
+  (is (= (d/parse-input "8A004A801A8002F478\n")
+         {:version 4
+          :subpackets [{:version 1
+                        :subpackets [{:version 5
+                                      :subpackets [{:version 6}]}]}]})))
