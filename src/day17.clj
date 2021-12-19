e29d92fa64830d5328b32a1ac3171259513467f0
Tyler Kindy <tylerkindy@gmail.com>
2021-12-19 17:16:51 -0500

    First stab at part 1
    This isn't giving me the right answer, even though I feel like my
    implementation is correct given the problem description. I must've
    missed something.

--- src/day17.clj
+++ src/day17.clj
@@ -14,8 +14,44 @@
 (defn read-input []
   (parse-input (slurp "inputs/day17.txt")))
 
-(defn part1 [target]
+(defn y-values-help [vy y]
+  (lazy-seq
+   (cons y (y-values-help (dec vy) (+ y vy)))))
+
+(defn y-values [vy0]
+  (y-values-help vy0 0))
+
+(defn relevant-y-values [vy0 min-range]
+  (->> vy0
+       y-values
+       (take-while (fn [y] (>= y min-range)))))
+
+(defn in-range? [n [min max]]
+  (<= min n max))
+
+(defn hits-y-range? [vy0 y-range]
+  (let [[min-range _] y-range]
+    (some (fn [y] (in-range? y y-range))
+          (relevant-y-values vy0 min-range))))
+
+(defn max-vy0 [y-range]
+  (->> (range)
+       (take-while (fn [vy0] (hits-y-range? vy0 y-range)))
+       last))
+
+;; TODO
+(defn max-vx0 [[min-x max-x]]
   0)
+
+(defn max-v0 [{:keys [x y]}]
+  [(max-vx0 x) (max-vy0 y)])
+
+(defn apex [vy0]
+  (/ (* vy0 (inc vy0)) 2))
+
+(defn part1 [target]
+  (let [[_ vy0] (max-v0 target)]
+    (apex vy0)))
 
 (defn -main []
   (let [target (read-input)]
--- test/day17_test.clj
+++ test/day17_test.clj
@@ -8,5 +8,30 @@
 (deftest parse-input
   (is (= example {:x [20 30] :y [-10 -5]})))
 
+(deftest hits-y-range?
+  (is (= (d/hits-y-range? 9 [-10 -5]) true)))
+
+(deftest relevant-y-values
+  (is (= (d/relevant-y-values 9 -10)
+         (list 0 9 17 24 30 35 39 42 44 45
+               45 44 42 39 35 30 24 17 9 0 -10)))
+  (is (= (take-last 3 (d/relevant-y-values 49 -150))
+         (list 0 -50 -101))))
+
+(deftest in-range?
+  (is (d/in-range? -150 [-150 -108])))
+
+(deftest max-vy0
+  (is (= (d/max-vy0 [-10 -5]) 9))
+  (is (= (d/max-vy0 [-150 -108]) 48)))
+
+(deftest apex
+  (is (= (d/apex 0) 0))
+  (is (= (d/apex 1) 1))
+  (is (= (d/apex 2) 3))
+  (is (= (d/apex 3) 6))
+  (is (= (d/apex 4) 10))
+  (is (= (d/apex 9) 45)))
+
 (deftest part1
   (is (= (d/part1 example) 45)))
